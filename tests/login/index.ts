cae9cdf097bdf7d9cc8fc7ffa9890d9c9cba7755
Tú Ank <tuank2301@gmail.com>
2025-03-24 18:22:48 +0700

    [feature]: import mailing

--- tests/login/index.ts
+++ tests/login/index.ts
@@ -1,19 +1,23 @@
 import {Page} from '@playwright/test';
-import * as path from "node:path";
+import * as path from 'node:path';
 
 const authFile = path.join(__dirname, '..', 'state.json');
 
-export const login = async (page:Page, url: string) => {
-    await page.goto(url);
-    if(page.url().startsWith('chrome-error://chromewebdata/')) {
-        await page.getByRole('button', { name: 'Advanced' }).click();
-        await page.getByRole('link', { name: 'Proceed to 10.255.58.201 (' }).click();
-        await page.locator("#username").fill(process.env.SSO_USERNAME);
-        await page.locator("#password").fill(process.env.PASSWORD);
-        await page.getByRole('button', { name: 'ĐĂNG NHẬP' }).click();
-        await page.waitForURL('http://localhost:8080/home-page');
-        await page.context().storageState({path: authFile});
-        await page.waitForTimeout(1000);
-        await page.goto(url);
+export const login = async (page: Page, url: string) => {
+  await page.goto('http://localhost:8080/');
+  await page.waitForTimeout(1000);
+  if (page.url().startsWith('chrome-error://chromewebdata/')) {
+    await page.getByRole('button', {name: 'Advanced'}).click();
+    await page.getByRole('link', {name: 'Proceed to 10.255.58.201 ('}).click();
+    await page.waitForTimeout(2000);
+    if (!page.url().startsWith('http://localhost:8080/')) {
+      await page.locator('#username').fill(process.env.SSO_USERNAME);
+      await page.locator('#password').fill(process.env.PASSWORD);
+      await page.getByRole('button', {name: 'ĐĂNG NHẬP'}).click();
+      await page.waitForURL('http://localhost:8080/home-page');
+      await page.context().storageState({path: authFile});
     }
+    await page.waitForTimeout(1000);
+    await page.goto(url);
+  }
 }
--- utils/fill.utils.ts
+++ utils/fill.utils.ts
@@ -2,32 +2,34 @@ import {Locator, Page} from '@playwright/test';
 import {ICreate} from '../constants/interface';
 
 export const fillValue = async (locator: Locator, data: ICreate[], page: Page) => {
-    for (const item of data) {
-        if (item.type === 'text') {
-            await locator.locator(`input[name="${item.field}"]`).fill(item.value);
-        } else if (item.type === 'select') {
-            const select = locator.locator(`div#${item.field}`);
-            await select.click();
-            await select.getByRole('searchbox').fill(item.value);
-            await select.locator('p-overlay').getByRole('listbox').locator('p-dropdownitem').first().click();
-        } else if (item.type === 'autocomplete') {
-            const autocomplete = locator.locator(`input[name="${item.field}"]`);
-            await autocomplete.fill('Giang Thị Nhung');
-            await autocomplete.locator('xpath=following-sibling::p-overlay').getByRole('option').first().click();
-        } else if (item.type === 'multiselect') {
-            const select = locator.locator(`div#${item.field}`);
-            const listOptions = item.value.split(',');
-            await select.click();
-            for (const op of listOptions) {
-                await page.waitForTimeout(200);
-                await select.getByRole('searchbox').first().fill(op);
-                await page.waitForTimeout(200);
-                await select.locator('p-overlay').getByRole('option').first().click();
-                await page.waitForTimeout(200);
-            }
-        } else if (item.type === 'date') {
-            await locator.locator(`input[name="${item.field}"]`).pressSequentially(item.value);
-            await page.pause();
-        }
+  for (const item of data) {
+    if (item.type === 'text') {
+      await locator.locator(`input[name="${item.field}"]`).fill(item.value);
+    } else if (item.type === 'select') {
+      const select = locator.locator(`div#${item.field}`);
+      await select.click();
+      await select.getByRole('searchbox').fill(item.value);
+      await select.locator('p-overlay').getByRole('listbox').locator('p-dropdownitem').first().click();
+    } else if (item.type === 'autocomplete') {
+      const autocomplete = locator.locator(`input[name="${item.field}"]`);
+      await autocomplete.fill('Giang Thị Nhung');
+      await autocomplete.locator('xpath=following-sibling::p-overlay').getByRole('option').first().click();
+    } else if (item.type === 'multiselect') {
+      const select = locator.locator(`div#${item.field}`);
+      const listOptions = item.value.split(',');
+      await select.click();
+      for (const op of listOptions) {
+        await page.waitForTimeout(200);
+        await select.getByRole('searchbox').first().fill(op);
+        await page.waitForTimeout(200);
+        await select.locator('p-overlay').getByRole('option').first().click();
+        await page.waitForTimeout(200);
+      }
+    } else if (item.type === 'date') {
+      await locator.locator(`input[name="${item.field}"]`).pressSequentially(item.value);
+      const datePickerCalendar = page.locator('[role="grid"].p-datepicker-calendar');
+      await datePickerCalendar.locator('span.p-highlight').first().click();
+      await page.pause();
     }
+  }
 }
